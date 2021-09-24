e76f9f1b52e6608f037a4a4f6f801343cddc2d35
TorbenCodingBots <91204421+TorbenCodingBots@users.noreply.github.com>
2021-09-24 12:13:21 -0400

    Fixed PasswordGen.py

--- PasswordGen.py
+++ PasswordGen.py
@@ -6,8 +6,12 @@ chars = 'abcdefghijklomnpqrstuvwxyz1234567890~!@#$%^&*()_+?<>-'
 length = input('gennerated password length:')
 length = int(length)
 
-for p in range(3):
+passwords = input('amount of passwords to geenerate:')
+passwords = int(passwords)
+
+for p in range(passwords):
   password = ''
   for c in range(length):
     password += random.choice(chars)
   print(password)
+
